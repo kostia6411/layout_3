96c273f7218435953ff5031ba0def84b88b719b9
kostia6411 <91974940+kostia6411@users.noreply.github.com>
2024-05-02 18:17:43 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+import os
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    id = 1
+    while id <= 10:
+        url = f"https://tululu.org/txt.php?id={id}"
+
+        response = requests.get(url)
+        response.raise_for_status()
+
+        os.makedirs("Books", exist_ok=True)
+
+        filename = f'Books/id{id}.txt'
+        with open(filename, 'wb') as file:
+            file.write(response.content)
+
+        id = id+1
