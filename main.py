69695863918615ad02b9231a22b1dc47136ae52f
kostia6411 <91974940+kostia6411@users.noreply.github.com>
2024-05-02 18:59:00 +0300

    Проверил наличие книги

--- main.py
+++ main.py
@@ -2,17 +2,26 @@ import requests
 import os
 
 
+def check_for_redirect(response):
+    if response.history:
+        raise requests.HTTPError
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    os.makedirs("Books", exist_ok=True)
     for id in range(10):
         url = f"https://tululu.org/txt.php?id={id}"
 
-        response = requests.get(url)
-        response.raise_for_status()
+        try:
 
-        os.makedirs("Books", exist_ok=True)
+            response = requests.get(url)
+            response.raise_for_status()
+            check_for_redirect(response)
 
-        filename = f'Books/id{id}.txt'
-        with open(filename, 'wb') as file:
-            file.write(response.content)
+            filename = f'Books/id{id}.txt'
+            with open(filename, 'wb') as file:
+                file.write(response.content)
+        except requests.HTTPError:
+            print("Книга не найдена")
+
+
 
