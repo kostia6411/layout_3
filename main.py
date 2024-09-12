d8939d5b4191ed9e42a1bc0f8f299ff86c302d17
kostia6411 <91974940+kostia6411@users.noreply.github.com>
2024-09-12 18:26:21 +0300

    download books with titles

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
 import requests
 import os
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
 
+
+def download_txt(filepath, response):
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
 
 def check_for_redirect(response):
     if response.history:
         raise requests.HTTPError
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     os.makedirs("Books", exist_ok=True)
-    for id in range(10):
+
+    for id in range(1, 10):
         url = f"https://tululu.org/txt.php?id={id}"
 
         try:
-
             response = requests.get(url)
             response.raise_for_status()
             check_for_redirect(response)
 
-            filename = f'Books/id{id}.txt'
-            with open(filename, 'wb') as file:
-                file.write(response.content)
+            response_author = requests.get(f"https://tululu.org/b{id}/")
+            soup = BeautifulSoup(response_author.text, 'lxml')
+            tag = soup.find('h1')
+            text = tag.text.split("::", maxsplit=1)
+
+            book_name = text[0].strip()
+            sort_book_name = sanitize_filename(book_name)
+            auhtor = text[1].strip()
+            print(book_name)
+            print(auhtor)
+
+            filepath = os.path.join('Books', f'{sort_book_name}.txt')
+            print(sort_book_name)
+            download_txt(filepath, response)
         except requests.HTTPError:
             print("Книга не найдена")
-
-
 
